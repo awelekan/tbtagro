58c214b63802db9d80b784fe33992c29740ef3e8
Muideen Awe <awe_lekan@yahoo.com>
2023-02-22 12:54:11 +0100

    Modefl commits

--- models/Product.js
+++ models/Product.js
@@ -1,25 +1,37 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
-const productSchema = new mongoose.Schema(
-  {
-    name: { type: String, required: true },
-    slug: { type: String, required: true, unique: true },
-    category: { type: String, required: true },
-    image: { type: String, required: true },
-    price: { type: Number, required: true },
-    brand: { type: String, required: true },
-    rating: { type: Number, required: true, default: 0 },
-    numReviews: { type: Number, required: true, default: 0 },
-    countInStock: { type: Number, required: true, default: 0 },
-    description: { type: String, required: true },
-    isFeatured: { type: Boolean, default: false },
-    banner: String,
-  },
-  {
-    timestamps: true,
-  }
+const reviewSchema = new mongoose.Schema(
+    {
+        name: { type: String, required: true },
+        comment: { type: String, required: true },
+        rating: { type: Number, required: true },
+    },
+    {
+        timestamps: true,
+    }
 );
 
-const Product =
-  mongoose.models.Product || mongoose.model("Product", productSchema);
+const productSchema = mongoose.Schema(
+    {
+        name: { type: String, required: true, unique: true },
+        seller: { type: mongoose.Schema.Types.ObjectID, ref: 'User' },
+        slug: { type: String, required: true, unique: true },
+        image: { type: String, required: true },
+        images: [String],
+        brand: { type: String, required: true },
+        category: { type: String, required: true },
+        description: { type: String, required: true },
+        price: { type: Number, required: true },
+        countInStock: { type: Number, required: true },
+        rating: { type: Number, required: true },
+        numReviews: { type: Number, required: true },
+        reviews: [reviewSchema],
+    },
+    {
+        timestamp: true
+    }
+);
+
+const Product = mongoose.model('Product', productSchema);
+
 export default Product;
